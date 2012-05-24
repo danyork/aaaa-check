091f72c18ed69ad923517e69a02a26e124f4a036
Dan York <dyork@lodestar2.com>
2012-05-24 19:06:13 -0400

    Added introductory header

--- aaaa-check.py
+++ aaaa-check.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
+#
+# aaaa-check.py
+#
+# Copyright 2012 Dan York
+# License: BSD
+#
+# A *very* simple program that loops forever to check whether manually input domain names
+# have IPv6 addresses (AAAA records).  Created out of the need to check whether domain names entered
+# into a web site had AAAA records.  The cleaner way to do this would be to build it into the 
+# website itself - and that may happen - but in the meantime I could copy domain names from the
+# web page and paste them into the terminal window where I had this app running.
+#
+# To terminate the program simply do a Ctrl+C
+#
+# Requires the dnspython library available from 
+#   http://www.dnspython.org/ or 
+#   https://github.com/rthalley/dnspython
+#
 
 import dns.resolver
-import dns.exception
 
 while 1:
     try: 
